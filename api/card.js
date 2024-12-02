dc30bc20ed28ba1bd5db4f9422dab87fc4879a86
mbido <ma.bidault39@gmail.com>
2024-12-02 20:44:02 +0100

    Mise à jour du SVG : réduction de la hauteur, ajustement de la position des éléments et modification de la taille de la police

--- api/card.js
+++ api/card.js
@@ -9,8 +9,8 @@ export default function handler(req, res) {
   const offset = circumference - (percentage / 100 * circumference);
 
   // Lecture du template HTML
-  const svg = `<svg width="450" height="220" xmlns="http://www.w3.org/2000/svg">
-    <rect x="1" y="1" width="448" height="218" fill="#141321" rx="5" stroke="#FFFFFF" stroke-width="1"/>
+  const svg = `<svg width="450" height="200" xmlns="http://www.w3.org/2000/svg">
+    <rect x="1" y="1" width="448" height="198" fill="#141321" rx="5" stroke="#FFFFFF" stroke-width="1"/>
     <defs>
       <path id="star-icon" d="M12 0l3.7 7.5 8.3 1.2-6 5.8 1.4 8.3-7.4-3.9-7.4 3.9 1.4-8.3-6-5.8 8.3-1.2z" />
     </defs>
@@ -27,10 +27,10 @@ export default function handler(req, res) {
       <path transform="translate(0, 89) scale(0.8)" fill="#F1E05A" d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11z" />
       <text x="30" y="100" font-size="14px" font-weight="600" fill="#A9FEF6" font-family="'Segoe UI', Ubuntu, 'Helvetica Neue', Sans-Serif">Year: <tspan x="170" y="100" fill="#D93B7D">${currentYear}</tspan></text>
     </g>
-    <g transform="translate(350, 110)">
+    <g transform="translate(350, 100)">
       <circle r="40" fill="none" stroke="#441E37" stroke-width="6" />
       <circle r="40" fill="none" stroke="#D93B7D" stroke-width="6" stroke-linecap="round" transform="rotate(-90)" stroke-dasharray="${circumference}" stroke-dashoffset="${offset}" />
-      <text x="0" y="15" text-anchor="middle" font-size="38px" font-weight="600" fill="#A9FEF6" font-family="'Segoe UI', Ubuntu, 'Helvetica Neue', Sans-Serif">${completedDays}</text>
+      <text x="0" y="15" text-anchor="middle" font-size="32px" font-weight="600" fill="#A9FEF6" font-family="'Segoe UI', Ubuntu, 'Helvetica Neue', Sans-Serif">${completedDays}</text>
     </g>
   </svg>`;
 
