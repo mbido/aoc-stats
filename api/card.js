55f2d6c8ae77fcf9d2a7dfcea907efec57b0918d
mbido <ma.bidault39@gmail.com>
2024-12-02 20:19:15 +0100

    meilleur affichage et suppression de ficheirs inutiles

--- api/card.js
+++ api/card.js
@@ -17,15 +17,15 @@ export default function handler(req, res) {
     <text x="30" y="45" font-size="26px" fill="#D93B7D" font-family="Arial, sans-serif" font-weight="bold">${username}'s AoC Stats</text>
     <g transform="translate(30, 85)">
       <use href="#star-icon" transform="translate(0, -16) scale(0.8)" fill="#F1E05A" />
-      <text x="30" font-size="16px" fill="#A9FEF6" font-family="Arial, sans-serif">Total stars: <tspan x="140" fill="#F1E05A">${totalStars}</tspan></text>
+      <text x="30" font-size="16px" fill="#A9FEF6" font-family="Arial, sans-serif">Total stars: <tspan x="170" fill="#F1E05A">${totalStars}</tspan></text>
       <use href="#star-icon" transform="translate(0, 14) scale(0.8)" fill="#F1E05A" />
-      <text x="30" y="30" font-size="16px" fill="#A9FEF6" font-family="Arial, sans-serif">Stars this year: <tspan x="140" y="30" fill="#F1E05A">${currentYearStars}</tspan></text>
+      <text x="30" y="30" font-size="16px" fill="#A9FEF6" font-family="Arial, sans-serif">Stars this year: <tspan x="170" y="30" fill="#F1E05A">${currentYearStars}</tspan></text>
       <path transform="translate(0, 44) scale(0.8)" fill="#F1E05A" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm0 18c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8zm.5-13H11v6l5.2 3.2.8-1.3-4.5-2.7V7z" />
-      <text x="30" y="60" font-size="16px" fill="#A9FEF6" font-family="Arial, sans-serif">Current day: <tspan x="140" y="60" fill="#A9FEF6">${currentDay}</tspan></text>
+      <text x="30" y="60" font-size="16px" fill="#A9FEF6" font-family="Arial, sans-serif">Current day: <tspan x="170" y="60" fill="#A9FEF6">${currentDay}</tspan></text>
       <path transform="translate(0, 74) scale(0.8)" fill="#F1E05A" d="M9 16.17l-4.17-4.17-1.42 1.41 5.59 5.59 12-12-1.41-1.41z" />
-      <text x="30" y="90" font-size="16px" fill="#A9FEF6" font-family="Arial, sans-serif">Completed days: <tspan x="140" y="90" fill="#A9FEF6">${completedDays}</tspan></text>
+      <text x="30" y="90" font-size="16px" fill="#A9FEF6" font-family="Arial, sans-serif">Completed days: <tspan x="170" y="90" fill="#A9FEF6">${completedDays}</tspan></text>
       <path transform="translate(0, 104) scale(0.8)" fill="#F1E05A" d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11z" />
-      <text x="30" y="120" font-size="16px" fill="#A9FEF6" font-family="Arial, sans-serif">Year: <tspan x="140" y="120" fill="#D93B7D">${currentYear}</tspan></text>
+      <text x="30" y="120" font-size="16px" fill="#A9FEF6" font-family="Arial, sans-serif">Year: <tspan x="170" y="120" fill="#D93B7D">${currentYear}</tspan></text>
     </g>
     <g transform="translate(390, 130)">
       <circle r="45" fill="none" stroke="#441E37" stroke-width="12" />
